4367cd02c532c8d8f20b8297896ef3ee3d87544b
Robert Smith <master_schmadoodle@outlook.com>
2023-06-11 01:08:17 +0100

    Added minor functionality

--- src/front/render.js
+++ src/front/render.js
@@ -3,8 +3,39 @@
  * author: ryvor (@ryvor)
  */
 const { ipcRenderer } = require('electron');
+const fs = require('fs');
+var untitledCount = 1;
+var documentID = 1;
 
-ipcRenderer.on('file-contents', (event, path, fileContents) => {
-	console.log('Received file contents:', fileContents);
-	console.log('Received file path:', path);
+ipcRenderer.on('contents', (event, path) => {
+	try {
+		const stats = fs.statSync(path);
+		let type = 0;
+		
+		if (stats.isFile()) {
+			type = 1;
+		} else if (stats.isDirectory()) {
+			type = 2;
+		}
+		if(type === 0) throw new Error('The path does not exist or is neither a file nor folder: '+path);
+		console.log('Path is type: '+type);
+	  } catch (error) {
+		// Handle the error if the path doesn't exist or other file system errors
+		console.error(error);
+	  }
+	
+});
+ipcRenderer.on('newFile', (event) => {
+	// Create new tab
+	const tab = document.createElement('tab');
+	const tab_title = document.createElement('span');
+	const tab_close = document.createElement('close-tab');
+	const tab_container = document.querySelector('tabs');
+	tab_title.textContent = 'Untitled-'+untitledCount;
+	tab.setAttribute('document-ID', documentID);
+	tab_container.appendChild(tab);
+	tab.appendChild(tab_title);
+	tab.appendChild(tab_close);
+	untitledCount++
+	// Create new notepad
 });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
+/*****************************************/
+/*               VARIABLES               */
+//#region ********************************/
+
 const { app, Menu, BrowserWindow, globalShortcut, dialog, ipcMain } = require('electron');
 const path = require('path');
-const fs = require('fs');
 const InDev = process.argv.includes('--forge');
+
+var currentWindow, mainWindow, aboutWindow;
 var keybinds_status = false;
+
+//#endregion *****************************/
+/*             SYSTEM EVENTS             */
+//#region ********************************/
 
 if(require('electron-squirrel-startup')) terminate(true);
 app.on('ready', createWindow);
 app.on('window-all-closed', terminate);
 app.on('activate', activate);
+app.on('blur', blur);
+app.on('focus', focus);
+app.on('window-all-closed', allWindowsClosed);
+app.on('browser-window-created', newWindow);
 
-var mainWindow, aboutWindow;
+//#endregion *****************************/
+/*            SYSTEM FUNCTIONS           */
+//#region ********************************/
 
 /** createWindow
  * Creates a new window and loads the configuration
@@ -22,7 +37,7 @@ function createWindow() {
 		title: process.env.productName,
 		titleBarStyle: 'hidden',
 		titleBarOverlay: {
-			color: '#2f3241',
+			color: '#00000000',
 			symbolColor: '#74b1be',
 			height: 20
 		},
@@ -32,8 +47,7 @@ function createWindow() {
 			preload: path.join(__dirname, 'preload.js'),
 		},
 	});
-	mainWindow.on('blur', ()=>blur());
-	mainWindow.on('focus', ()=>focus());
+	registerKeybinds();
 	mainWindow.loadFile(path.join(__dirname, 'front/index.html'));
 	registerApplicationMenu();
 
@@ -42,14 +56,55 @@ function createWindow() {
 		mainWindow.webContents.openDevTools();
 	}
 };
-function focus() {
-	log('Window focused');
-	registerKeybinds();
+/** Terminate
+ * Closes the program in windows and linux, but waits for all of the  CMD+Q command on MAC
+ */
+function terminate(force=false) {
+	if (process.platform!=='darwin'|| force) {
+		globalShortcut.unregisterAll();
+		app.quit();
+	}
 }
+/** activate
+ * This function is called when the application has been initially launched or attempted to launch whilst already open
+ */
+function activate() {
+	if (BrowserWindow.getAllWindows().length === 0) createWindow();
+}
+/** blur
+ * This this function is executed when the main browser window has lost focus
+ */
 function blur() {
-	log('Window lost focus');
 	unregisterKeybinds();
 }
+/** focus
+ * This this function is executed when the main browser window has gained focus
+ */
+function focus() {
+	registerKeybinds();
+}
+/** allWindowsClosed
+ * This function resets the current window if all of the windows have been closed
+ */
+function allWindowsClosed() {
+	currentWindow = null;
+}
+/** newWindow
+ * This function sets the current window each time you change/create windows and unsets the window when you leave it.
+ */
+function newWindow(event, window) {
+	window.on('focus', () => {
+		currentWindow = window;
+	});
+	window.on('blur', () => {
+		currentWindow = null;
+	});
+}
+
+//#endregion *****************************/
+/*               FUNCTIONS               */
+//#region ********************************/
+
 /** registerApplicationMenu
  * This function sets the application alt menu for the program
  */
@@ -132,63 +187,41 @@ function registerApplicationMenu() {
 		}
 	]));
 }
-
-/** Terminate
- * Closes the program in windows and linux, but waits for all of the  CMD+Q command on MAC
- */
-function terminate(force=false) {
-	if (process.platform!=='darwin'|| force) {
-		globalShortcut.unregisterAll();
-		app.quit();
-	}
-}
-
-/** activate
- * Creates a new window if there isnt already one existing.
- */
-function activate() {
-	if (BrowserWindow.getAllWindows().length === 0) createWindow();
-}
-
 /** registerKeybinds
  * Registers the keybinds once
  */
 function registerKeybinds() {
-	if(!globalShortcut.register('Alt+A', ()=>{console.log('TODO')})) console.log('Failed to register global shortcut 1');
-	if(!globalShortcut.register('CommandOrControl+H', ()=>{console.log('TODO')})) console.log('Failed to register global shortcut 2');
-	if(!globalShortcut.register('CommandOrControl+Shift+H', ()=>{console.log('TODO')})) console.log('Failed to register global shortcut 3');
-	if(process.platform==='darwin') {
-		if(!globalShortcut.register('Command+Q', ()=>terminate(true))) console.log('Failed to register global shortcut 4.1');
+	if(keybinds_status === false) {
+		if(!globalShortcut.register('Alt+A', ()=>{console.log('TODO')})) log('Failed to register global shortcut 1');
+		if(!globalShortcut.register('CommandOrControl+H', ()=>{console.log('TODO')})) log('Failed to register global shortcut 2');
+		if(!globalShortcut.register('CommandOrControl+Shift+H', ()=>{console.log('TODO')})) log('Failed to register global shortcut 3');
+		if(process.platform==='darwin')
+			if(!globalShortcut.register('Command+Q', ()=>terminate(true)))
+				log('Failed to register global shortcut 4');
+		//---//
+		if(!globalShortcut.register('CommandOrControl+N', ()=>{currentWindow.webContents.send('newFile')})) log('Failed to register global shortcut 5');
+		if(!globalShortcut.register('CommandOrControl+Shift+N', ()=>{console.log('TODO')})) log('Failed to register global shortcut 6');
+		if(!globalShortcut.register('CommandOrControl+O', ()=>openDialog(1))) log('Failed to register global shortcut 7');
+		if(!globalShortcut.register('CommandOrControl+Shift+O', ()=>openDialog(2))) log('Failed to register global shortcut 8');
+		//---//
+		if(!globalShortcut.register('CommandOrControl+Z', ()=>{console.log('TODO')})) log('Failed to register global shortcut 9');
+		if(!globalShortcut.register('CommandOrControl+Shift+Z', ()=>{console.log('TODO')})) log('Failed to register global shortcut 10');
+		if(!globalShortcut.register('CommandOrControl+S', ()=>{console.log('TODO')})) log('Failed to register global shortcut 11');
+		if(!globalShortcut.register('CommandOrControl+Shift+S', ()=>{console.log('TODO')})) log('Failed to register global shortcut 12');
+		keybinds_status = true;
 	}
-	//---//
-	if(!globalShortcut.register('CommandOrControl+N', ()=>{console.log('TODO')})) console.log('Failed to register global shortcut 5');
-	if(!globalShortcut.register('CommandOrControl+Shift+N', ()=>{console.log('TODO')})) console.log('Failed to register global shortcut 6');
-	if(!globalShortcut.register('CommandOrControl+O', ()=>openDialogue(1))) console.log('Failed to register global shortcut 7');
-	if(!globalShortcut.register('CommandOrControl+Shift+O', ()=>openDialogue(2))) console.log('Failed to register global shortcut 8');
-	//---//
-	if(!globalShortcut.register('CommandOrControl+Z', ()=>{console.log('TODO')})) console.log('Failed to register global shortcut 9');
-	if(!globalShortcut.register('CommandOrControl+Shift+Z', ()=>{console.log('TODO')})) console.log('Failed to register global shortcut 10');
-	if(!globalShortcut.register('CommandOrControl+S', ()=>{console.log('TODO')})) console.log('Failed to register global shortcut 11');
-	if(!globalShortcut.register('CommandOrControl+Shift+S', ()=>{console.log('TODO')})) console.log('Failed to register global shortcut 12');
 }
-/**
- * 
+/** unregisterKeybinds
+ * This function will unbing any keybindings made.
  */
 function unregisterKeybinds() {
-	if(keybinds_status) {
+	if(keybinds_status === true) {
 		globalShortcut.unregisterAll();
+		keybinds_status = false;
 	}
 }
-  
-/**
- * 
- */
-function unregisterKeybinds() {
-	globalShortcut.unregisterAll();
-}
-  
-/**
- * 
+/** openDialog
+ * This function will open a window to enable the ability to open a file or folder
  */
 function openDialog(type) {
 	switch(type) {
@@ -205,27 +238,19 @@ function openDialog(type) {
 		if (!result.canceled && result.filePaths.length > 0) {
 			const path = result.filePaths[0];
 			log('Open '+type+': '+path);
-			if(type=1) {
-				mainWindow.webContents.send('file-contents', path, fs.readFileSync(path, 'utf-8'));
-			} else {
-
-			}
+			currentWindow.webContents.send('contents', path);
 		}
 	}).catch(err => {
 		console.log('Error opening '+type+':', err);
 	});
 }
-
-/** openAbout
- * 
- */
-function openAbout() {
-	console.log('todo');
-}
-
 /** log
  * This function writes a string to the log file with the logging format.
  */
 function log() {
 
 }
+
+//#endregion *****************************/
+/*                  EOF                  */
+/*****************************************/
